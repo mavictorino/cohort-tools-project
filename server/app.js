548b9bb5ccca6f5ebbb58a17cf750cbf010fb3a1
“Samy <samy.mouahbi@hotmail.com>
2024-11-11 16:40:43 +0100

    Install React app and Express Server and Cohort middleware

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
+const cors = require("cors");
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
@@ -15,6 +16,11 @@ const app = express();
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
+app.use(
+  cors({
+    origin: ['http://localhost:5173']
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
@@ -25,8 +31,22 @@ app.use(cookieParser());
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 // ...
+
+
+const cohorts = require("./cohorts.json")
+const students = require("./students.json")
+
+
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
+});
+
+app.get("/api/cohorts", (req, res) => {
+  res.json(cohorts);
+});
+
+app.get("/api/students", (req, res) => {
+  res.json(students);
 });
 
 
